b185e9bd6bf4fc4f6d888a5b38591db4f190e0bf
Bartosz Wawrzyniak <baartek98@gmail.com>
2022-03-20 20:01:39 +0100

    quickSort

--- main.c
+++ main.c
@@ -1,89 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//############################
-//#     MERGE SORT !!!!!!!   #
-//############################
+// ###################
+// #     QUICKSORT   #
+// ###################
 
-void merge(int tab[], int l_side, int middle, int r_side)
+void quickSort(int tab[], int start_element, int last_element)
 {
-    int i, j, k;
-    int n1 = middle - l_side + 1;
-    int n2 = r_side - middle;
-    int L_temp[n1], R_temp[n2];
-
-
-    for (i = 0; i < n1; i++)
-    {
-        L_temp[i] = tab[l_side + i];
-    }
-
-    for (j = 0; j < n2; j++)
-    {
-        R_temp[j] = tab[middle + 1 + j];
-    }
-
-
-    i = 0;
-    j = 0;
-    k = l_side;
-    while (i < n1 && j < n2)
-        {
-            if (L_temp[i] <= R_temp[j])
-            {
-                tab[k] = L_temp[i];
-                i++;
-            }
-            else
-            {
-                tab[k] = R_temp[j];
-                j++;
-            }
-        k++;
-        }
-
-
-    while (i < n1)
-        {
-            tab[k] = L_temp[i];
-            i++;
-            k++;
-        }
-
-    while (j < n2)
-        {
-            tab[k] = R_temp[j];
-            j++;
-            k++;
-        }
+    if (last_element <= start_element) return;
+    int pivot = partitionFunction(tab, start_element, last_element);
+    quickSort(tab,start_element, pivot - 1);
+    quickSort(tab, pivot + 1, last_element);
 }
 
-
-void mergeSort(int tab[], int l_side, int r_side)
+int partitionFunction(int tab[], int start_element, int last_element)
 {
-    if (l_side < r_side) {
+    int i = start_element - 1;
+    int pivot = tab[last_element];
 
-        int middle = l_side + (r_side - l_side) / 2;
+    for(int k = start_element; k <= last_element; k++)
+        {
+        if(tab[k] < pivot)
+         {
+            i++;
+			int temp = tab[i];
+			tab[i] = tab[k];
+			tab[k] = temp;
+         }
+        }
+        i++;
+        int temp = tab[i];
+        tab[i] = tab[last_element];
+        tab[last_element] = temp;
 
-
-        mergeSort(tab, l_side, middle);
-        mergeSort(tab, middle + 1, r_side);
-        merge(tab, l_side, middle, r_side);
-    }
+        return i;
 }
 
 int main()
 {
-    int tab[] = { 4, 13, 5325, 1231, 23, 5, 9, -1 };
+    int tab[] = {2, 4, 5, 10, 23, 65, 1000, 13, -982};
     int tab_size = sizeof(tab) / sizeof(tab[0]);
 
-    mergeSort(tab, 0, tab_size - 1);
+    quickSort(tab,0,tab_size - 1);
 
     printf("Wynik sortowania tablicy: \n");
     for (int i = 0; i < tab_size; i++)
     {
         printf("%d ", tab[i]);
     }
-
     return 0;
 }
